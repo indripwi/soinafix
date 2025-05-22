2bdf3b04289de2f44b72a00eca40e1a85a104505
unknown <natalinoveniastritelaumbanua>
2025-05-23 00:13:42 +0800

    Menambahkan model Announcement

---
+++ app/Models/Announcement.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Announcement extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'gambar_url',
+        'pdf_file',
+        'created_at',
+    ];
+
+     /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
